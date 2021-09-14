e6a191d9b999fc753071217105569e2dea3a79fc
Tony Bowden <tony@tmtm.com>
2021-09-14 10:57:03 +0300

    Generate diff WD/official

---
+++ bin/diff.rb
@@ -0,0 +1,20 @@
+#!/bin/env ruby
+# frozen_string_literal: true
+
+require 'every_politician_scraper/comparison'
+
+# Standardise data
+class Comparison < EveryPoliticianScraper::Comparison
+  REMAP = {
+    'Conservative Party of Canada' => 'Conservative',
+    'Liberal Party of Canada' => 'Liberal',
+    'New Democratic Party' => 'NDP',
+  }.freeze
+
+  def wikidata_csv_options
+    { converters: [->(val) { REMAP.fetch(val, val) }] }
+  end
+end
+
+diff = Comparison.new('wikidata/results/current-members.csv', 'data/official.csv').diff
+puts diff.sort_by { |r| [r.first, r[1].to_s] }.reverse.map(&:to_csv)
